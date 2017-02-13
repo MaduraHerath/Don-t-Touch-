d215c5fcc1f16de6f453aad1cd6a6dce2ec9fb20
v1shva <vishva.sudantha@gmail.com>
2017-02-13 18:09:35 +0530

    line collider

--- Don-t Touch/Assets/BounceBall.cs
+++ Don-t Touch/Assets/BounceBall.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public class BounceBall : MonoBehaviour {
+    public float speed = 30;
+    // Use this for initialization
+    void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
---
+++ Don-t Touch/Assets/DrawLine.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+public class DrawLine : MonoBehaviour
+{
+    private LineRenderer line; // Reference to LineRenderer
+    private GameObject LineObject;
+    private Vector3 mousePos;
+    private Vector3 startPos;    // Start position of line
+    private Vector3 endPos;    // End position of line
+    void Update()
+    {
+
+        // On mouse down new line will be created
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (line == null)
+                createLine();
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            line.SetPosition(0, mousePos);
+            startPos = mousePos;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (line)
+            {
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0;
+                line.SetPosition(1, mousePos);
+                endPos = mousePos;
+                addColliderToLine();
+                line = null;
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            if (line)
+            {
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0;
+                line.SetPosition(1, mousePos);
+            }
+        }
+    }
+    // Following method creates line runtime using Line Renderer component
+    private void createLine()
+    {
+        LineObject = new GameObject("Line");
+        line = LineObject.AddComponent<LineRenderer>();
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        line.sortingOrder = 0;
+        line.SetVertexCount(2);
+        line.SetWidth(0.1f, 0.1f);
+        line.SetColors(Color.black, Color.black);
+        line.useWorldSpace = true;
+    }
+    // Following method adds collider to created line
+    private void addColliderToLine()
+    {
+        //LineObject.AddComponent<BoxCollider>();
+        LineObject.AddComponent<CapsuleCollider>();
+    }
+}
